a6638da63e86a3581cb024744f8ff9a0b522e7e7
lbnihi <94848042+lbnihi@users.noreply.github.com>
2023-01-27 21:28:00 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -34,32 +34,7 @@ int main(int argc, char const *argv[]) {
 }
 
 
-        //b.EmprunterLivre("ti","idlecteur");
-        //b.afficherLivresLecteur("12345678910","idLecteur");
-         //b.restituerlivre("idLecteur","1234567891");
-         //b.pourcentageEmprunt();
-         //b.afficherLivresAuteur();
-        //b.afficherLivres();
-        /*Date a_day(7,31,2022);
- a_day.updateYear(-2000);
-  std::cout << "a year: " << a_day.year() << std::endl;
-  std::cout << "a day: " << toString(a_day) << std::endl;
-  std::cout << "#day: " << dayOfYear(a_day) << std::endl;
-  a_day.next();
-
-  std::cout << "a day + 1: " << toString(a_day) << std::endl;
-  std::cout << "#day: " << dayOfYear(a_day) << std::endl;*/
-
- //Book my_book("title","nom","prenom","12345","langue","type",Date (12,3,2018),"12345678910",Date (1,1,1),{"sjsjk"});
-    //cout<<my_book.getLecteur();
-  /*Emprunt mehdi("1234567891");
-     mehdi.emprunter("123456",Date (12,3,2018));
-     cout<<mehdi.getstatus()<<endl;
-     cout<<mehdi.getidLecteur()<<endl;
-     mehdi.rendre();
-     cout<<mehdi.getstatus()<<endl;
-     cout<<mehdi.getidLecteur();*/
-   
+        
     
 
 
